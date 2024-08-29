bcbe1f15c912fb5329d1dd8781b3241216782be3
Zachai Griffin <griffzac000@warren.k12.in.us>
2024-08-29 09:53:54 -0400

    Initial commit

---
+++ starwars.py
@@ -0,0 +1,4 @@
+name = input("What is your name: ")
+droids = input("what is the number of droids?")
+wookiees = input("what is the number of wookiees?")
+print(name, "wants to meet", droids, "droids and ", wookiees, "Wookiees!")
